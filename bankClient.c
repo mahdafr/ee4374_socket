3683e15abe86a32915e8c0a7492805abb412ff92
Mahdokht Af <mmafravi@gmail.com>
2017-05-09 21:54:15 -0600

    maybe now i can get a good test

--- bankClient.c
+++ bankClient.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
 				break;
 		}
 		//get the account number and value
-		printf("Args: %u %u %u\n",(u_int) argv[2][0],(u_int) argv[3],(u_int) argv[4]);
+		printf("Args: %s %u %u\n",(char) argv[2][0],(unsigned int) argv[3],(unsigned int) argv[4]);
 		toSend->acctnum = (unsigned int) argv[3];
 		toSend->value = (unsigned int) argv[4];
 		
